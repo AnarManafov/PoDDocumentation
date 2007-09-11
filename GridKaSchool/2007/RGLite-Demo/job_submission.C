fc7707b36b82b151414796924f25b3a3818fb658
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-09-11 08:56:02 +0000

    D-Grid Doc: added RGLite Demo
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/Documentation@1254 47ffe4a8-6206-0410-815e-8f101de37557

---
+++ GridKaSchool/2007/RGLite-Demo/job_submission.C
@@ -0,0 +1,62 @@
+/************************************************************************/
+/*! \file job_submission.C
+	RGlite test macro - Job submission *//*
+
+         version number:    $LastChangedRevision: 1160 $
+         created by:        Anar Manafov
+                            2006-11-13
+         last changed by:   $LastChangedBy: manafov $ $LastChangedDate: 2007-08-08 16:31:05 +0200 (Wed, 08 Aug 2007) $
+
+         Copyright (c) 2006 GSI GridTeam. All rights reserved.
+*************************************************************************/
+//
+// Example of usage:
+// root [1] .x tests/job_submission.C("/home/anar/jdl/test.jdl")
+//
+
+// STD
+#include <iostream>
+// RGlite
+#include "TGridJobStatus.h"
+
+using namespace std;
+
+const size_t MAX_ITER_COUNT = 50;
+const size_t SLEEP_TIME = 20000;// in milliseconds
+
+Int_t job_submission( const char *_JDL )
+{
+  TGrid::Connect("glite");
+  
+  TGridJob *job = gGrid->Submit( _JDL );
+  if( !job )
+    {
+      cerr << "TGridJob is NULL." << endl;
+      return 1;
+    }
+  
+  TGridJobStatus *status = job->GetJobStatus();
+  if( !status )
+    {
+      cerr << "TGridJobStatus is NULL." << endl;
+      return 1;
+    }
+  
+  for( size_t i = 0; i < MAX_ITER_COUNT; ++i )
+    {
+      cout << " - - - - - - - - - - - - - " << endl;
+      cout << "Iteration #" << i << endl;
+      switch ( status->GetStatus() )
+	{
+	case TGridJobStatus::kDONE:
+	  job->GetOutputSandbox("/tmp");
+	  return 0;
+	case TGridJobStatus::kABORTED:
+	case TGridJobStatus::kFAIL:
+	  return 1;
+	default:
+	  gSystem->Sleep( SLEEP_TIME );
+	}
+    }
+  return 1;
+}

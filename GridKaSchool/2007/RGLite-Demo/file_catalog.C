655a5b33a69ed6cfef5e93e743c1bf752b4d66f0
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-09-13 12:14:44 +0000

    formatting
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/Documentation@1262 47ffe4a8-6206-0410-815e-8f101de37557

--- GridKaSchool/2007/RGLite-Demo/file_catalog.C
+++ GridKaSchool/2007/RGLite-Demo/file_catalog.C
@@ -1,6 +1,6 @@
 /************************************************************************/
 /*! \file file_catalog.C
-	RGlite test macro - File Catolog operations */ /*
+ RGlite test macro - File Catolog operations */ /*
 
          version number:    $LastChangedRevision$
          created by:        Anar Manafov
@@ -22,116 +22,116 @@
 // helper function
 void list_dir( Bool_t _NeedAll )
 {
-	TGridResult * result = gGrid->Ls();
-	if( _NeedAll )
-	{
-		result->Print( "all" );
-		delete result;
-		return;
-	}
+    TGridResult * result = gGrid->Ls();
+    if ( _NeedAll )
+    {
+        result->Print( "all" );
+        delete result;
+        return;
+    }
 
-	Int_t i = 0;
-	while ( result->GetFileName( i ) )
-	{
-		cout << "File: " << result->GetFileName( i++ ) << endl;
-	}
-	delete result;
+    Int_t i = 0;
+    while ( result->GetFileName( i ) )
+    {
+        cout << "File: " << result->GetFileName( i++ ) << endl;
+    }
+    delete result;
 }
 
 void NextPhase( Bool_t _NeedPause, const std::string &_strDescr, size_t _nPhaseNum )
 {
-	if( _NeedPause )
-	{
-		cout << "Press \"Enter\" to continue..." << endl;
-		getchar();
-	}
-	cout << endl;
-	cout << "<<<<[Phase #" << _nPhaseNum << "]>>>> " << _strDescr << endl;
+    if ( _NeedPause )
+    {
+        cout << "Press \"Enter\" to continue..." << endl;
+        getchar();
+    }
+    cout << endl;
+    cout << "<<<<[Phase #" << _nPhaseNum << "]>>>> " << _strDescr << endl;
 }
 
 int file_catalog()
 {
-	Bool_t bNeedPause = kFALSE;
-	size_t count = 0;
+    Bool_t bNeedPause = kFALSE;
+    size_t count = 0;
 
-	cout << "This is a test Macro for RGlite File Catalog operations" << endl;
+    cout << "This is a test Macro for RGlite File Catalog operations" << endl;
 
-	const char * const pszErrorString( "Test: FAILED" );
-	const char * const pszOKString( "Test: OK" );
+    const char * const pszErrorString( "Test: FAILED" );
+    const char * const pszOKString( "Test: OK" );
 
-	NextPhase( bNeedPause, "Init of gLite API Wrapper", ++count );
-	TGrid::Connect( "glite" );
+    NextPhase( bNeedPause, "Init of gLite API Wrapper", ++count );
+    TGrid::Connect( "glite" );
 
-	if ( gGrid )
-	{
-		string dir( "/grid" );
-		NextPhase( bNeedPause, "Changing current working dir to " + dir, ++count );
-		if ( !gGrid->Cd( dir.c_str() ) )
-		{
-			cout << pszErrorString << endl;
-			return 1;
-		}
+    if ( gGrid )
+    {
+        string dir( "/grid" );
+        NextPhase( bNeedPause, "Changing current working dir to " + dir, ++count );
+        if ( !gGrid->Cd( dir.c_str() ) )
+        {
+            cout << pszErrorString << endl;
+            return 1;
+        }
 
-		NextPhase( bNeedPause, "Asking for a PWD", ++count );
-		cout << "Working Directory is " << gGrid->Pwd() << endl;
+        NextPhase( bNeedPause, "Asking for a PWD", ++count );
+        cout << "Working Directory is " << gGrid->Pwd() << endl;
 
-		NextPhase( bNeedPause, "Listing current dir", ++count );
-		list_dir( kFALSE );
+        NextPhase( bNeedPause, "Listing current dir", ++count );
+        list_dir( kFALSE );
 
-		dir = "/grid/dech";
-		NextPhase( bNeedPause, "Changing current working dir to " + dir, ++count );
-		if ( !gGrid->Cd( dir.c_str() ) )
-		{
-			cout << pszErrorString << endl;
-			return 1;
-		}
+        dir = "/grid/dech";
+        NextPhase( bNeedPause, "Changing current working dir to " + dir, ++count );
+        if ( !gGrid->Cd( dir.c_str() ) )
+        {
+            cout << pszErrorString << endl;
+            return 1;
+        }
 
-		NextPhase( bNeedPause, "Listing current dir", ++count );
-		list_dir( kFALSE );
+        NextPhase( bNeedPause, "Listing current dir", ++count );
+        list_dir( kFALSE );
 
-		const string newfile( "root_file_test10.file" );
-		const TUUID uuid;
-		const string strGUID( uuid.AsString() );
-		const string strURL( "lfn://grid0.gsi.de/test/test3/" + newfile );
-		const string strServer( "grid0.gsi.de" );
+        const string newfile( "root_file_test10.file" );
+        const TUUID uuid;
+        const string strGUID( uuid.AsString() );
+        const string strURL( "lfn://grid0.gsi.de/test/test3/" + newfile );
+        const string strServer( "grid0.gsi.de" );
 
 
-		NextPhase( bNeedPause, "Creating the file: "+ newfile, ++count );
-		if ( !gGrid->Register( newfile.c_str(), strURL.c_str(), 0, strServer.c_str(), strGUID.c_str() ) )
-		{
-			cout << pszErrorString << endl;
-			return 1;
-		}
+        NextPhase( bNeedPause, "Creating the file: " + newfile, ++count );
+        if ( !gGrid->Register( newfile.c_str(), strURL.c_str(), 0, strServer.c_str(), strGUID.c_str() ) )
+        {
+            cout << pszErrorString << endl;
+            return 1;
+        }
 
-		NextPhase( bNeedPause, "Listing current dir", ++count );
-		list_dir( kFALSE );
+        NextPhase( bNeedPause, "Listing current dir", ++count );
+        list_dir( kFALSE );
 
-		NextPhase( bNeedPause, "Removing file: " + newfile, ++count );
-		if ( !gGrid->Rm( newfile.c_str() ) )
-		{
-			cout << pszErrorString << endl;
-			return 1;
-		}
+        NextPhase( bNeedPause, "Removing file: " + newfile, ++count );
+        if ( !gGrid->Rm( newfile.c_str() ) )
+        {
+            cout << pszErrorString << endl;
+            return 1;
+        }
 
-		NextPhase( bNeedPause, "Listing current dir", ++count );
-		list_dir( kFALSE );
+        NextPhase( bNeedPause, "Listing current dir", ++count );
+        list_dir( kFALSE );
 
-		NextPhase( bNeedPause, "Checking whether we can reconnection to LFC after a timeout", ++count );
-		cout << "sleeping for a 1 min..." << endl;
-		gSystem->Sleep( 60000 );
-		cout << "waking up and continuing tests..." << endl;
+        NextPhase( bNeedPause, "Checking whether we can reconnection to LFC after a timeout", ++count );
+        cout << "sleeping for a 1 min..." << endl;
+        gSystem->Sleep( 60000 );
+        cout << "waking up and continuing tests..." << endl;
 
-		NextPhase( bNeedPause, "Listing current dir with 'all' details", ++count );
-		list_dir( kTRUE );
+        NextPhase( bNeedPause, "Listing current dir with 'all' details", ++count );
+        list_dir( kTRUE );
 
-		cout << "---------------------------" << endl;
-		cout << pszOKString << endl;
-		cout << "---------------------------" << endl;
-		return 0;
-	}
-	else
-	{
-		cout << pszErrorString << endl;
-		return 1;
-	}
+        cout << "---------------------------" << endl;
+        cout << pszOKString << endl;
+        cout << "---------------------------" << endl;
+        return 0;
+    }
+    else
+    {
+        cout << pszErrorString << endl;
+        return 1;
+    }
 }
--- GridKaSchool/2007/RGLite-Demo/job_submission.C
+++ GridKaSchool/2007/RGLite-Demo/job_submission.C
@@ -1,6 +1,6 @@
 /************************************************************************/
 /*! \file job_submission.C
-	RGlite test macro - Job submission *//*
+ RGlite test macro - Job submission *//*
 
          version number:    $LastChangedRevision$
          created by:        Anar Manafov
@@ -26,37 +26,37 @@ const size_t SLEEP_TIME = 20000;// in milliseconds
 
 Int_t job_submission( const char *_JDL )
 {
-  TGrid::Connect("glite");
-  
-  TGridJob *job = gGrid->Submit( _JDL );
-  if( !job )
+    TGrid::Connect("glite");
+
+    TGridJob *job = gGrid->Submit( _JDL );
+    if ( !job )
     {
-      cerr << "TGridJob is NULL." << endl;
-      return 1;
+        cerr << "TGridJob is NULL." << endl;
+        return 1;
     }
-  
-  TGridJobStatus *status = job->GetJobStatus();
-  if( !status )
+
+    TGridJobStatus *status = job->GetJobStatus();
+    if ( !status )
     {
-      cerr << "TGridJobStatus is NULL." << endl;
-      return 1;
+        cerr << "TGridJobStatus is NULL." << endl;
+        return 1;
     }
-  
-  for( size_t i = 0; i < MAX_ITER_COUNT; ++i )
+
+    for ( size_t i = 0; i < MAX_ITER_COUNT; ++i )
     {
-      cout << " - - - - - - - - - - - - - " << endl;
-      cout << "Iteration #" << i << endl;
-      switch ( status->GetStatus() )
-	{
-	case TGridJobStatus::kDONE:
-	  job->GetOutputSandbox("/tmp");
-	  return 0;
-	case TGridJobStatus::kABORTED:
-	case TGridJobStatus::kFAIL:
-	  return 1;
-	default:
-	  gSystem->Sleep( SLEEP_TIME );
-	}
+        cout << " - - - - - - - - - - - - - " << endl;
+        cout << "Iteration #" << i << endl;
+        switch ( status->GetStatus() )
+        {
+            case TGridJobStatus::kDONE:
+                job->GetOutputSandbox("/tmp");
+                return 0;
+            case TGridJobStatus::kABORTED:
+            case TGridJobStatus::kFAIL:
+                return 1;
+            default:
+                gSystem->Sleep( SLEEP_TIME );
+        }
     }
-  return 1;
+    return 1;
 }
